fbfc54ef7dc660448ba8b18b786b17cca28feffa
Bill <41483625+mapsedge@users.noreply.github.com>
2025-05-30 11:50:14 -0500

    Update and rename Ctrl+Num7.py to insert-random-number.py

--- coding_scripts/insert-random-number.py
+++ coding_scripts/insert-random-number.py
@@ -1,6 +1,10 @@
+# insert a random number of -length- digits.
+
+length = 6
+
 def getLabel():
     secs = str(int(time.time() * 1000000))  # Convert to microseconds and keep as integer
-    return secs[-6:]
+    return secs[-length:]
 
 label = getLabel()
 # label = label.replace('.', '0')
